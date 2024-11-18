18c981d02c89ec47f7ab651c07f10ec9e78064a3
danishts <danishsaddique519@gmail.com>
2024-11-18 16:45:51 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// In your React component (e.g., App.js)
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="font-norwester">
+      <h1 className="text-4xl">Welcome to My React App!</h1>
+      <p className="text-lg">Unleash the Power of Fan Content in 
+Sports Broadcasting</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+
+
+
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        norwester: ["Norwester", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
