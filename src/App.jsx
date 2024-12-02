804f801447c7f354806b3e01b8f42d48c321b4e9
danishts <danishsaddique519@gmail.com>
2024-12-02 16:07:19 +0500

    FANAl88

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,67 @@
+// import React from 'react';
 
-import React from "react";
-import About from "./About";
-import Features from "./Features";
-import ContactUs from "./ContactUs";
-import Navbar from "./Navbar"; // Ensure file and path casing match
-import Dashboard from "./Dashboard";
+// import About from './About';
+// import Features from './Features';
+// import ContactUs from './ContactUs';
+// import Navbar from './Navbar.JSX';
+// import Dashboard from './Dashboard';
+
+
+// function App() {
+//   return (
+//     <div>
+//       {/* Navbar */}
+//       <Navbar/>
+      
+//       {/* Page Sections */}
+//       <div id="dashboard">
+//         <Dashboard/>
+//       </div>
+//       <div id="about">
+//         <About />
+//       </div>
+//       <div id="features">
+//         <Features />
+//       </div>
+//       <div id="contact-us">
+//         <ContactUs />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+
+import About from './About';
+import Features from './Features';
+import ContactUs from './ContactUs';
+ // Ensure file and path casing match
+import Dashboard from './Dashboard';
+import Navbar from './Navbar.JSX';
 
 function App() {
   return (
     <div>
       {/* Navbar */}
       <Navbar />
-
+      
       {/* Page Sections */}
       <div id="dashboard">
         <Dashboard />
