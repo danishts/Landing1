3624a315b896a4271f5f00fecd9fa26c57efd907
danishts <danishsaddique519@gmail.com>
2024-12-02 15:56:40 +0500

    FANAl88

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,5 @@
-// import React from 'react';
-
-// import About from './About';
-// import Features from './Features';
-// import ContactUs from './ContactUs';
-// import Navbar from './Navbar.JSX';
-// import Dashboard from './Dashboard';
-
-
-// function App() {
-//   return (
-//     <div>
-//       {/* Navbar */}
-//       <Navbar/>
-      
-//       {/* Page Sections */}
-//       <div id="dashboard">
-//         <Dashboard/>
-//       </div>
-//       <div id="about">
-//         <About />
-//       </div>
-//       <div id="features">
-//         <Features />
-//       </div>
-//       <div id="contact-us">
-//         <ContactUs />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import React from "react";
-
 import About from "./About";
 import Features from "./Features";
 import ContactUs from "./ContactUs";
